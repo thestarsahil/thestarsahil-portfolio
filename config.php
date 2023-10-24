ee40b000966e513ca2b58359c81f240aecc701a4
Sahil Ali <84167034+thestarsahil@users.noreply.github.com>
2023-10-24 05:55:03 +0000

    php file added

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Database configuration
+ */
+$db_config = [
+    'host' => 'tcp:mysqlserver784.database.windows.net,1433',
+    'database' => 'mySampleDatabase',
+    'username' => 'thestarsahil',
+    'password' => '{your_password}',
+];
+
+return $db_config;
